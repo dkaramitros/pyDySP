d5712c295f8db02aed3ba5bb863083766855e7cc
Dimitris Karamitros <d.karamitros@bristol.ac.uk>
2024-03-23 16:36:43 +0000

    added arias

--- pydaq.py
+++ pydaq.py
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 class Test:
     
     def __init__(self):
-        # Info
-        self.name = "Default test"
-        self.description = "This is the default test"
-        self.filename = "N/A"
-        self.time = "N/A"
-        self.no_channels = 0
-        # Data
+        self.set_test_info(
+            name = "Default test",
+            description = "This is the default test.",
+            filename = "N/A",
+            time = "N/A",
+            no_channels = 0
+        )
         self.channel = []
-        pass
 
     def set_test_info(self, name: str=None, description: str=None, filename: str=None, time: str=None, no_channels: int=None):
         if name != None: self.name = name
@@ -51,16 +50,20 @@ class Test:
                 raw_data = imported_data[f'chan{i+1}'].flatten()
             )
     
-    def remove_offset(self, points: int=1000):
+    def baseline(self, **kwargs):
         for channel in self.channel:
-            channel.remove_offset(points=points)
+            channel.baseline(**kwargs)
     
-    def trim_threshold(self, **kwargs):
-            [start_0,end_0] = self.channel[0].trim_threshold(**kwargs)
+    def trim(self, **kwargs):
+            [start_0,end_0] = self.channel[0].trim(**kwargs)
+            for kwarg in ["trim_method", "start", "end"]:
+                if kwarg in kwargs:
+                    del kwargs[kwarg]
             for channel in self.channel[1:]:
-                channel.trim_points(start=start_0, end=end_0, **kwargs)
+                channel.trim(trim_method="Points", start=start_0, end=end_0, **kwargs)
 
-    def plot(self, channels: np.ndarray = None, columns: int = 1, plot_type: str="Timehistory", **kwargs):
+    def plot(self, channels: np.ndarray = None, columns: int = 1, plot_type: str="Timehistory",
+        description: bool=False, **kwargs):
         if channels is None:
             channels = np.arange(self.no_channels)
         no_channels = len(channels)
@@ -77,44 +80,65 @@ class Test:
 class Channel:
     
     def __init__(self):
-        pass
+        self.set_channel_info(
+            name = "Default channel",
+            description = "This is the default channel.",
+            unit = "Undefined unit",
+            calibration = 1
+        )
+        self.set_channel_data(
+            raw_time = np.zeros(2),
+            raw_data = np.zeros(2)
+        )
     
-    def set_channel_info(self, name: str="", description: str="", unit: str="", calibration: float=1):
-        self.name = name
-        self.description = description
-        self.unit = unit
-        self.calibration = calibration
+    def set_channel_info(self, name: str=None, description: str=None, unit: str=None, calibration: float=None):
+        if name != None: self.name = name
+        if description != None: self.description = description
+        if unit != None: self.unit = unit
+        if calibration != None: self.calibration = calibration
 
     def set_channel_data(self, raw_time: np.ndarray, raw_data: np.ndarray):
         self._raw_time = raw_time
-        self._time = raw_time
         self._raw_data = raw_data
+        self._raw_points = np.size(self._raw_data)
+        self._raw_timestep = self._raw_time[1] - self._raw_time[0]
+        self._time = raw_time
         self._data = raw_data
+        self._points = np.size(self._time)
+        self._timestep = self._time[1] - self._time[0]
 
     def reset_raw_data(self):
         self._time = self._raw_time
         self._data = self._raw_data
+        self._points = self._raw_points
+        self._timestep = self._raw_timestep
 
-    def remove_offset(self, points: int=1000):
+    def baseline(self, points: int=1000):
         self._data = self._raw_data - np.average(self._raw_data[:points])
 
-    def trim_points(self, start: int=None, end: int=None, buffer: int=100, time_shift: bool=True):
+    def trim(self, buffer: int=100, time_shift: bool=True, trim_method: str="Threshold",
+        start: int=0, end: int=0, threshold_ratio: float=0.05, threshold_acc: float=0.01):
+        if self._points < self._raw_points:
+            self.reset_raw_data()
+        match trim_method:
+            case "Points":
+                pass
+            case "Threshold":
+                threshold = min([
+                    threshold_ratio * np.amax(np.abs(self._data)),
+                    threshold_acc / self.calibration
+                ])
+                start = np.argmax(np.abs(self._data) > threshold)
+                end = np.size(self._data) - np.argmax(np.abs(np.flip(self._data)) > threshold)
+            case "Arias":
+                [start,end] = self.arias[3]
         start = max([start - buffer, 0])
         end = min([end + buffer, np.size(self._time)])
         self._time = self._time[start:end]
         self._data = self._data[start:end]
+        self._points = np.size(self._time)
         if time_shift == True:
             self._time -= self._time[0]
-        return [start,end]
-
-    def trim_threshold(self, ratio: float=0.05, max_threshold: float=0.01, **kwargs):
-        threshold = min([
-            ratio * np.amax(np.abs(self._data)),
-            max_threshold / self.calibration
-        ])
-        start = np.argmax(np.abs(self._data) > threshold)
-        end = np.size(self._data) - np.argmax(np.abs(np.flip(self._data)) > threshold)
-        self.trim_points(start=start, end=end, **kwargs)
         return [start,end]
 
     def timehistory(self):
@@ -124,30 +148,50 @@ class Channel:
     
     def fourier(self):
         [t,y] = self.timehistory()
-        no_t = np.size(t)
-        dt = t[1] - t[0]
-        no_f = int( 2**(no_t-1).bit_length() )
-        s = np.abs( np.fft.rfft(a=y, n=no_f) )
-        f = np.fft.rfftfreq(n=no_f,d=dt)
+        _no_freqs = int( 2**(self._points-1).bit_length() )
+        s = np.abs( np.fft.rfft(a=y, n=_no_freqs) )
+        f = np.fft.rfftfreq(n=_no_freqs, d=self._timestep)
         return np.array([f,s])
 
-    def plot(self, plot_type: str="Timehistory", axis=None, **kwargs):
+    def arias(self, g: float=9.81):
+        arias = sp.integrate.cumulative_trapezoid(
+            x=self._time,
+            y=np.pi/2/9.81 * (g * self._data/self.calibration)**2
+        )
+        arias = np.append(arias,arias[-1])
+        start = np.argmax(arias > 0.05*arias[-1])
+        end = np.argmax(arias > 0.95*arias[-1])
+        duration = self._time[end] - self._time[start]
+        return arias[-1], duration, [self._time,arias], [start,end]
+
+    def plot(self, plot_type: str="Timehistory", name: bool=True, description: bool=True, axis=None, **kwargs):
         if axis == None:
             figure, axis = plt.subplots()
         match plot_type:
             case "Timehistory":
                 [x,y] = self.timehistory()
-                axis.set_xlabel("Time (sec)")
-                axis.set_ylabel(self.name + " (" + self.unit + ")")
+                xlabel = "Time (sec)"
+                ydesc = "Timehistory (" + self.unit + ")"
             case "Fourier":
                 [x,y] = self.fourier()
-                axis.set_xlabel("Frequency (Hz)")
-                axis.set_ylabel(self.name)
+                xlabel = "Frequency (Hz)"
+                ydesc = "Fourier Amplitude"
                 if "xlim" in kwargs:
                     xlim = kwargs["xlim"]
                 else:
                     xlim = 50
                 axis.set_xlim(0,xlim)
+            case "Arias":
+                [x,y] = self.arias()[2]
+                xlabel = "Time (sec)"
+                ydesc = "Arias Intensity (m/s)"
         axis.plot(x,y)
+        ylabel = ""
+        if name:
+            ylabel += self.name
+        if description:
+            ylabel +=  " " + ydesc
+        axis.set_xlabel(xlabel)
+        axis.set_ylabel(ylabel)
         axis.grid()
         return axis
