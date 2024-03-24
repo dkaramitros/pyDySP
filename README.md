e0097a57d09ed47a6000216de73d36d7b5744493
Dimitris Karamitros <d.karamitros@bristol.ac.uk>
2024-03-24 01:13:26 +0000

    added transfer functions

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Data Acquisition with Python
 ## Information
 
 Python classes used to process laboratory data from the Shaking Table experimental facilities at the University of Bristol.
+
+## Instructions
+
+The file `pydaq.py` includes two classes:
+
+The `Test` class provides methods to add, manage, and plot data from multiple channels. Transfer functions can also be produced and analyzed.
+
+The `Channel` class provides methods for signal processing, including baseline correction, filtering, and trimming. Plotting methods are also provided.
+
+Example Jupyter notebooks are provided.
--- pydaq.py
+++ pydaq.py
@@ -75,30 +75,44 @@ class Test:
                 self.channel[channels[i]].plot(axis=axis, description=description, **kwargs)
         return axes
     
-    def transfer(self, channel_from: int=0, channel_to: int=1, h_method: int=1,
-        plot: bool=True, axis=None, xlim: float=50, **kwargs):
+    def transfer(self, channel_from: int=0, channel_to: int=1, h_method: int=1, axis=None,
+        find_peak: bool=True, find_damping: bool=True, xlim: float=50, **kwargs):
+        # Plot
+        if axis == None:
+            figure, axis = plt.subplots()
+        axis.set_xlabel("Frequency (Hz)")
+        axis.set_ylabel("Transfer Function "+self.channel[channel_to].name+"/"+self.channel[channel_from].name)
+        axis.set_xlim(0,xlim)
+        axis.grid()
+        # Transfer function
         if h_method == 1:
             [f,Pxy] = sp.signal.csd(x=self.channel[channel_from]._data, y=self.channel[channel_to]._data,
                 fs=1/self.channel[channel_from]._timestep, **kwargs)
             [_,Pxx] = sp.signal.welch(x=self.channel[channel_from]._data,
                 fs=1/self.channel[channel_from]._timestep, **kwargs)
-            transfer = np.abs(Pxy / Pxx)
+            t = np.abs(Pxy / Pxx)
         else:
             [f,Pyy] = sp.signal.welch(x=self.channel[channel_to]._data,
                 fs=1/self.channel[channel_from]._timestep, **kwargs)
             [_,Pxy] = sp.signal.csd(x=self.channel[channel_from]._data, y=self.channel[channel_to]._data,
                 fs=1/self.channel[channel_from]._timestep, **kwargs)
-            transfer = np.abs(Pyy / Pxy)
-        if plot:
-            if axis == None:
-                figure, axis = plt.subplots()
-            axis.plot(f,transfer,label=self.name)
-            axis.set_xlabel("Frequency (Hz)")
-            axis.set_ylabel("Transfer Function "+self.channel[channel_to].name+"/"+self.channel[channel_from].name)
-            axis.set_xlim(0,xlim)        
-            return axis
-        else:
-            return [f,transfer]
+            t = np.abs(Pyy / Pxy)
+        base_plot, = axis.plot(f,t,label=self.name)
+        # Peak
+        if find_peak or find_damping:
+            index_n = np.argmax(t)
+            f_n = f[index_n]
+            t_n = t[index_n]
+            axis.plot(f_n, t_n, "o", color=base_plot.get_color())
+        # Damping (half-bandwidth method)
+        if find_damping:
+            t_hb = max(t_n/np.sqrt(2),t[0])
+            eqn = sp.interpolate.interp1d(f,t-t_hb)
+            f_1 = sp.optimize.root_scalar(eqn, bracket=[0,f_n], method='bisect').root
+            f_2 = sp.optimize.root_scalar(eqn, bracket=[f_n,2*f_n], method='bisect').root
+            ksi = (f_2-f_1) / (2*f_n)
+            axis.plot([f_1,f_2], [t_hb,t_hb], "--", color=base_plot.get_color())
+        return axis,f,t,f_n,ksi
 
 
 class Channel:
