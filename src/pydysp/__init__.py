bdf1b2dac9ce34783192b300c04286573532210e
Dimitris Karamitros <d.karamitros@bristol.ac.uk>
2024-06-25 23:17:49 +0100

    updated versionning

--- src/pydysp/__init__.py
+++ src/pydysp/__init__.py
@@ -3,8 +3,4 @@ from . import test
 from . import channel
 
 # Basic information
-__author__ = "Dimitris Karamitros"
-__copyright__ = "Copyright 2024 Dimitris Karamitros"
-__license__ = "MIT"
-__title__ = "pyDySP"
 __version__ = "0.1.0"
