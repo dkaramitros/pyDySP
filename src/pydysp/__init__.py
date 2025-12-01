dd4ccabc31391a2c67887c30fd7d2a88a6769cb9
Dimitris Karamitros <d.karamitros@bristol.ac.uk>
2025-12-01 19:10:19 +0000

    added helper imports

--- src/pydysp/__init__.py
+++ src/pydysp/__init__.py
@@ -4,13 +4,28 @@ from .test import Test
 from .spectra import FourierSpectrum, WelchSpectrum
 from .arias import AriasResult
 from .response import ResponseSpectrum
+from .helpers import (
+    annotate_peak,
+    annotate_time_peak,
+    annotate_fourier_peak,
+    annotate_psd_peak,
+    annotate_response_peak,
+)
 
 __all__ = [
+    # Main classes
     "Channel",
     "Test",
+    # Additional classes
     "FourierSpectrum",
     "WelchSpectrum",
     "AriasResult",
     "ResponseSpectrum",
+    # Helper functions
+    "annotate_peak",
+    "annotate_time_peak",
+    "annotate_fourier_peak",
+    "annotate_psd_peak",
+    "annotate_response_peak",
 ]
 __version__ = "0.2.0"
